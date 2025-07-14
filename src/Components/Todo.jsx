54ef98eda751738a373f165d2f61c05cb120690a
Obiora Kingsley <kingsleyobiora527@gmail.com>
2025-07-14 09:45:53 +0100

    initial commit

---
+++ src/Components/Todo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoItems from "./TodoItems";
+
+const Todo = () => {
+  return (
+    <div>
+      <TodoItems />
+    </div>
+  );
+};
+
+export default Todo;
---
+++ src/Components/TodoItems.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoItems = () => {
+  return <div>Todo</div>;
+};
+
+export default TodoItems;
