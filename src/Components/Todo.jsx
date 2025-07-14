a7ae290bf088b8a315357d0b57cce37edc6f5069
Obiora Kingsley <kingsleyobiora527@gmail.com>
2025-07-14 11:52:00 +0100

    create header section in Todo.jsx and import TodoItems.jsx component

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import listicon from "../assets/images/list.png";
 import TodoItems from "./TodoItems";
 
 const Todo = () => {
   return (
-    <div>
+    <div className="todo-container">
+      <header className="header">
+        <nav className="navbar">
+          <img src={listicon} alt="" width="40" height="40" />
+          <h1>To-Do List</h1>
+        </nav>
+        <div className="todo-input">
+          <input type="text" />
+          <button>Add</button>
+        </div>
+      </header>
       <TodoItems />
     </div>
   );
--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import checked from "../assets/images/checked.png";
+import unchecked from "../assets/images/unchecked.png";
+import del from "../assets/images/delete.png";
 
 const TodoItems = () => {
-  return <div>Todo</div>;
+  return (
+    <main className="main">
+      <div className="todo-item">
+        <div className="item">
+          <img src={checked} alt="" width="18" />
+          <p>Hello</p>
+        </div>
+        <img src={del} alt="" width="22" />
+      </div>
+    </main>
+  );
 };
 
 export default TodoItems;
